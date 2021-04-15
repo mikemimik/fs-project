b4f5529c1dee29350b717b9de2d25b859911ada7
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-15 03:14:02 -0400

    feat(src): refactored App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,160 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import {
+  ListGroup,
+} from 'react-bootstrap';
+
 import './App.css';
 
-import List from './List';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    )
+  }
+}
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
+
+class ProjectExample extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/posts/:postId" component={Post} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+class Posts extends Component {
+  constructor () {
+    super();
+    this.state = {
+      posts: [],
+    };
+  }
+
+  fetchPosts = async () => {
+    try {
+      const response = await fetch('/posts');
+
+      if (response.ok) {
+        const data = await response.json();
+
+        this.setState({
+          posts: data,
+        });
+      } else {
+        console.warn('the response was bad');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  componentDidMount () {
+    this.fetchPosts();
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        <h2>Posts</h2>
+        <ListGroup>
+          {
+            posts.map((post) => {
+              return (
+                <ListGroup.Item key={post._id}>
+                  <Link to={`/posts/${post._id}`}>{post.excerpt}</Link>
+                </ListGroup.Item>
+              );
+            })
+          }
+        </ListGroup>
+      </div>
+    );
+  }
+}
+
+class Post extends Component {
+  constructor () {
+    super();
+    this.state = {
+      post: {},
+    };
+  }
+
+  fetchPost = async (postId) => {
+    try {
+      const response = await fetch(`/posts/${postId}`);
+
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({
+          post: data,
+        });
+      } else {
+        console.warn('the response was bad');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  componentDidMount () {
+    console.log('this.props:', this.props);
+    const { match } = this.props;
+    const { postId } = match.params;
+
+    this.fetchPost(postId);
+  }
+
+  render() {
+    const { post } = this.state;
+    return (
+      <div>{JSON.stringify(post)}</div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <List />
+      <ProjectExample />
     </div>
   );
 }
