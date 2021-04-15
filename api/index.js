6543687308f54d447e08061ef3f28070bb6e34b9
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-15 03:12:04 -0400

    feat(api): refactored index

--- api/index.js
+++ api/index.js
@@ -2,28 +2,31 @@ require('dotenv').config({
   path: `${__dirname}/.env`
 });
 
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// Logging Function
-function logEverything (req) {
-  console.log(
-    'path:', req.path,
-    'method:', req.method,
-    'params:', req.params,
-    'body:', req.body,
-  );
-}
+const { logger } = require('./utils');
+const { Post } = require('./models');
+const { postRouter } = require('./routes');
 
-app.use('/api', (req, res) => {
-  console.group('handling request: /api');
-  logEverything(req);
-  console.groupEnd();
+app.use(express.json());
+app.use('/posts', postRouter);
 
-  res.json({
-    data: []
-  });
+const uri = 'mongodb://localhost:27017/junocollege';
+
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+.then(() => {
+  console.log(`Successfull connected to: ${uri}`);
+})
+.catch((err) => {
+  console.error(`Failed to connect: ${uri}`);
+  console.error(err);
 });
 
 app.listen(process.env.PORT, () => {
